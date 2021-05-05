bc55b61dbf29bdeb3c0b2b59d49eaff55ab2173c
Seth <seth.a.gehring@gmail.com>
2021-05-05 12:53:37 -0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -30,4 +30,4 @@ cd $default_dir || exit
 unzip -j ${filename}
 eos=$(ls $default_dir | grep eos)
 tar=$(ls -Art $default_dir | tail -n 1)
-echo -e "You must now run: cvpi restore cvp $tar $eos"
+echo -e "For the latest restore run: cvpi restore cvp $tar $eos"
