c0afb3939e3c749b83df6d4abf62a0bd19f2a740
Seth <seth.a.gehring@gmail.com>
2021-06-24 11:19:45 -0700

    Try s3 url - to .

--- upgrade.sh
+++ upgrade.sh
@@ -91,7 +91,7 @@ wget -O "${filename}" --header="Host: ${bucket}.s3.amazonaws.com" \
 --header="Date: ${date}" \
 --header="Content-Type: ${contentType}" \
 --header="Authorization: AWS ${awsAccess}:${signature}" \
-"https://${bucket}.s3-us-west-2.amazonaws.com/${file}" || echo -en "Failed to get the version ""${version}"" from the s3 bucket" && exit 1
+"https://${bucket}.s3.us-west-2.amazonaws.com/${file}" || echo -en "Failed to get the version ""${version}"" from the s3 bucket" && exit 1
 
 # Confirmation
 read -r -p "Ready to upgrade from ${CVP_VERSION} to ${version}? (y/n):" response
